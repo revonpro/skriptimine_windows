664647fbe77290e232d50bfbfb9660d8887eec64
revonpro <revon.tammur@voco.ee>
2022-11-08 15:42:00 +0200

    Lokaalne kasutaja etteantud parooli ja nimega

---
+++ Lokaalne kasutaja.ps1
@@ -0,0 +1,4 @@
+﻿# Loome kasutaja jaoks parooli
+$KasutajaParool = ConvertTo-Securestring 'Parool1' -AsPlainText -Force
+# Toome kasutaja
+New-LocalUser 'Kasutaja1' -Password $KasutajaParool -Fullname 'Esimene kasutaja' -Description 'Local account'
