21bca7f3c63f53aadaee236ff9315193405e6580
revonpro <revon.tammur@voco.ee>
2022-11-09 11:20:29 +0200

    Lokaalne kasutaja käsurea parameetritega

---
+++ lokaalne kasutaja parameetritega.ps1
@@ -0,0 +1,10 @@
+﻿# vajalikud parameetrid
+param(
+$Kasutajanimi,
+$Taisnimi,
+$Kontokirjeldus
+)
+# loome kastaja jaoks parooli 
+$Kasutajaparool = ConvertTo-SecureString 'Parool1' -AsPlainText -Force
+#loome kasutaja
+New-LocalUser $Kasutajanimi -Password $Kasutajaparool -FullName $Taisnimi -Description $Kontokirjeldus
