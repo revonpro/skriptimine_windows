68074924278d5905f15790c83944b91d91121255
revonpro <revon.tammur@voco.ee>
2022-11-09 11:43:29 +0200

    kasutusjuhend vale käivitamise puhul

--- lokaalne kasutaja parameetritega.ps1
+++ lokaalne kasutaja parameetritega.ps1
@@ -1,10 +1,13 @@
-﻿# vajalikud parameetrid
-param(
-$Kasutajanimi,
-$Taisnimi,
-$Kontokirjeldus
-)
-# loome kastaja jaoks parooli 
+﻿    #kontroll parameetrite arvule 
+if ($args.Count -ne 3){
+    echo ".\skriptifailinimi.ps1 kasutajanimi taisnimi kirjeldus" 
+} else {
+    # vajalikud parameetrid
+    $Kasutajanimi = $args[0]
+    $Taisnimi = $args[1]
+    $Kontokirjeldus = $args[2]
+    # loome kastaja jaoks parooli 
 $Kasutajaparool = ConvertTo-SecureString 'Parool1' -AsPlainText -Force
-#loome kasutaja
+    # loome kasutaja
 New-LocalUser $Kasutajanimi -Password $Kasutajaparool -FullName $Taisnimi -Description $Kontokirjeldus
+}
