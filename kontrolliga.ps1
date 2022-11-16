9df79eb2dd66e1f7a1d4f9be3946b29e38490c7b
revonpro <revon.tammur@voco.ee>
2022-11-16 16:05:05 +0200

    kontrolliga.ps1

---
+++ kontrolliga.ps1
@@ -0,0 +1,12 @@
+﻿# loome_kasutaja_ja_parool
+$kasutajaParool = ConvertTo-Securestring "Parool1!" -AsPlainText -Force
+# küsime kasutaja andmed
+$Eesnimi = Read-Host "Sisesta oma Eesnimi"
+$Perenimi = Read-Host "Sisesta oma Perenimi"
+# loome_kasutajanimi
+$kasutajanimi = $Eesnimi.ToLower() + "." + $Perenimi.ToLower()
+# taisnime loomine
+$Taisnimi = $Eesnimi + "." + $Perenimi
+# kontokirjeldus
+$kasutajakirjeldus = "lokaalne_konto -" + $kasutajanimi
+New-localUser $kasutajanimi -Password $kasutajaparool -FullName $Taisnimi -Description $kasutajakirjeldus
