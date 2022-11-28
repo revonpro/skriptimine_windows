7217fb2908b57dd9faad7d2d9a31498c7140f8b0
revonpro <revon.tammur@voco.ee>
2022-11-28 10:59:29 +0200

    ad_kasutaja_kustutamine2.ps1

---
+++ ad_kasutaja_kustutamine2.ps1
@@ -0,0 +1,48 @@
+﻿#---------------------------------
+echo "Sisestage oma eesnimi:"
+    $eesnimi = Read-Host
+echo "Sisestage oma perenimi:"
+    $perenimi = Read-Host
+#---------------------------------
+$ErrorActionPreference = "SilentlyContinue"
+#---------------------------------
+$kasutajanimi = $eesnimi + "." + $perenimi
+$kasutajanimi = $kasutajanimi.ToLower()
+$kasutajanimi = TransLit($kasutajanimi)
+
+Remove-ADUser $kasutajanimi -Confirm:$false 
+#--------------------------------
+    if (!$?)
+    {
+    echo "Kasutaja $kasutajanimi ei eksisteeri"
+    }
+    else
+    {
+    echo "Kasutaja $kasutajanimi on kustutatud edukalt!"
+    }
+#--------------------------------
+# Täpitähe muutmine
+function Translit {
+    param(
+        [string] $inputString
+    )
+    #
+        $TransLit = @{
+        [char]'ä' = "a"
+        [char]'ö' = "o"
+        [char]'ü' = "u"
+        [char]'õ' = "o"
+        }
+    #
+    $outputString=""
+    #
+    foreach ($character in $inputCharacter = $inputString.ToCharArray())
+    {
+        if ($TransLit[$character] -cne $Null ){
+            $outputString += $TransLit[$character]
+        } else {
+            $outputString += $character
+        }
+    }
+    write-Output $outputString
+}
